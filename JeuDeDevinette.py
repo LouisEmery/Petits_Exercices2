1bfd67afd747e670d169c7e2d25e4672375a4641
LouisEmery <112487894+LouisEmery@users.noreply.github.com>
2022-10-07 16:36:01 -0400

    Update JeuDeDevinette.py

--- JeuDeDevinette.py
+++ JeuDeDevinette.py
@@ -1,13 +1,20 @@
 import random
-#choisir un nombre au hazard
-nbr = random.randint(0, 1000)
+nbr = 99999
 #definir choix
 choix = -1
 #definir le nombre d'essai
 nbrEssais = 0
-#commencer le jeu
-print("J'ai choisi un nombre entre 0 et 1000, essai de le trouver")
-while choix != nbr:
+play_again = "y"
+#definir les varables de choix
+x = 0
+y = 1000
+def choice():
+    global x
+    global y
+    global play_again
+    global nbrEssais
+    global nbr
+    global choix
     #demender le choix du joueur
     choix = int(input("quel est votre choix?"))
     #ajouter un essai
@@ -20,3 +27,24 @@ while choix != nbr:
     #s'il réussit
     else:
         print("Vous avez réussits en", nbrEssais, "essais!")
+        nbrEssais = 0
+    #demander a l'usager s'il veut rejouer
+        play_again = input("voulez vous rejouer? y/n")
+
+        return play_again
+
+while play_again != "n":
+    print("je choisi un nombre entre", x, "et", y,)
+    #demander s'il veut changer les limites du nombre au hazard
+    changement_nombre = input("voulez vous changer les limites du nombre? y/n")
+    if changement_nombre == "y":
+        #s'il veut, les changer
+        x = int(input("vous voulez un chiffre entre"))
+        y = int(input("et"))
+    else:
+        nbr = random.randint(x, y)
+        # commencer le jeu
+        print("J'ai choisi un nombre entre", x, "et", y, "essai de le trouver")
+        choix = -1
+        while choix != nbr:
+            choice()
